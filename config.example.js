e50f4db3adee339db67a48c0a48e3907e98c33d3
Peter Romijn <peter@samebestdevelopment.nl>
2020-01-17 16:42:34 +0100

    Added example config

---
+++ config.example.js
@@ -0,0 +1,20 @@
+module.exports = {
+  db: {
+    host: "",
+    database: "",
+    user: "",
+    password: "",
+  },
+  html: "Lorem upsum",
+  subject: "subject",
+  intervalTimer: 2000,
+  mail: {
+    host: "smtp.mail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "",
+      pass: "",
+    },
+  },
+}
